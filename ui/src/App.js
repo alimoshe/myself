039a52381214750953cfffb9eb2b3d09620a3eea
asalehi <engsalehi@live.com>
2022-04-24 23:52:27 +0430

    Add Layout

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import {Breadcrumb, ConfigProvider, Layout, Menu} from "antd";
 import './App.css';
+import faIR from 'antd/lib/locale/fa_IR';
+import 'moment/locale/fa';
+import moment from "moment";
+import {Content, Footer, Header} from "antd/es/layout/layout";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    moment.locale('fa');
+    return (
+        <ConfigProvider direction={"rtl"} locale={faIR} componentSize={"large"}>
+            <div>
+
+
+                <Layout className="layout">
+                    <Header>
+                        <div className="logo" />
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['2']}
+                            items={[{key: 1, label:'test1'},{key:2,label:'محصولات و خدمات'}]}
+                        />
+                    </Header>
+                    <Content style={{ padding: '0 50px' }}>
+
+                    </Content>
+
+                </Layout>
+            </div>
+        </ConfigProvider>
+    );
 }
 
 export default App;
