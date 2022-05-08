f907bb79d5ace5085a6640fbc93160d608ddf941
alimoshe <engsalehi@live.com>
2022-05-08 13:34:08 +0430

    Add Components folder and change main Page Layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import {ConfigProvider, Layout, Menu} from "antd";
+
+import faIR from 'antd/lib/locale/fa_IR';
+import 'moment/locale/fa';
+import moment from "moment";
 import './App.css';
+import {UserOutlined} from "@ant-design/icons";
+import {Content, Header} from "antd/es/layout/layout";
+
+const items = [
+    {
+        key: 1,
+        label: 'فروشندگان کالا'
+    },
+    {
+        key: 2,
+        label: 'تامین کنندگان خدمات'
+    },
+    {
+        key: 3,
+        style:{float:"left"},
+        label: 'فروشندگان کالا'
+    },];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    moment.locale('fa');
+    return (
+        <ConfigProvider locale={faIR} componentSize={"large"} direction={"rtl"}>
+            <Layout style={{backgroundColor:"white"}}>
+                <Header>
+                    <div className="logo">
+                        <UserOutlined size={128} />
+                    </div>
+                    <Menu
+
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        items={items}
+                    />
+                </Header>
+                <Content style={{padding: '0 50px'}}>
+
+                    <div className="site-layout-content">Content</div>
+                </Content>
+
+            </Layout>
+        </ConfigProvider>
+    );
 }
 
 export default App;
